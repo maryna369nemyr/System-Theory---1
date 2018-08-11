24949d230691d0f1779a3a9704b3005fa57aee77
Maryna Nemyrovska <42252398+maryna369nemyr@users.noreply.github.com>
2018-08-11 18:03:41 +0200

    outer folder

---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using ZedGraph;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draw
+{
+    
+
+    public partial class Form1 : Form
+    {
+
+        static public bool enable = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = enable;
+            button_output.Enabled = enable;
+           
+        }
+
+        private void CreateGraph(ZedGraphControl zgc)
+        {
+            GraphPane myPane = zgc.GraphPane;
+            PointPairList list = new PointPairList();
+            myPane.CurveList.Clear();
+            for (int i = 0; i < Program.y.Count; i++)
+            {
+                list.Add(i, Program.y[i]);
+            }
+            myPane.Title = "";
+            LineItem myCurve = myPane.AddCurve("Result",
+                  list, Color.Purple, SymbolType.None);
+
+            zgc.AxisChange();
+            zgc.Invalidate();
+            zgc.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            CreateGraph(zedGraphControl1);
+        }
+
+        private void Enter_k_Click(object sender, EventArgs e)
+        {
+
+
+   
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void button_init_Click(object sender, EventArgs e)
+        {
+            enable = false;
+
+            string s_q = textBox_q.Text;
+            string s_b = textBox_b.Text;
+            string s_a1 = textBox_a1.Text;
+            string s_a2 = textBox_a2.Text;
+            string s_T = textBox_T.Text;
+            string s_x0 = textBox_x0.Text, s_x1 = textBox_x1.Text, s_x2 = textBox_x2.Text;
+
+            if (!Processing.checkString(s_q, s_b, s_a1, s_a2, s_T, s_x0, s_x1, s_x2)) { MessageBox.Show(" The fields are empty."); }
+
+            else
+            {
+                try
+                {
+
+                    Processing.q = Int32.Parse(s_q);
+
+                    Processing.a1 = double.Parse(s_a1);
+                    Processing.a2 = double.Parse(s_a2);
+                    Processing.b = double.Parse(s_b);
+                    Processing.T = double.Parse(s_T);
+                    Processing.x_0.M[0][0] = double.Parse(s_x0);
+                    Processing.x_0.M[1][0] = double.Parse(s_x1);
+                    Processing.x_0.M[2][0] = double.Parse(s_x2);
+
+
+
+
+
+                    Program.k = (int)(30 / Processing.T);
+
+                    if (!Processing.checkComponents(Processing.q, Processing.a1, Processing.a2, Processing.T)) MessageBox.Show(" Incorrect parametres. Please, try to input again.");
+                    else
+                    {
+                        Processing.function();
+                        if (!Processing.checkLimit) MessageBox.Show(" Limit doesn't exist . Please, Enter correct T.");
+           
+                        enable = true;
+                    }
+
+                    button1.Enabled = enable;
+                    button_output.Enabled = enable;
+
+                }
+                catch (Exception ex)
+                {
+                    enable = false;
+                    MessageBox.Show(ex.Message);
+                    button1.Enabled = enable;
+                    button_output.Enabled = enable;
+                }
+            }
+
+
+
+
+
+
+        }
+
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+        }
+
+        private void label_k_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_q_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_output_Click(object sender, EventArgs e)
+        {
+
+
+            
+                Form2 form = new Form2();
+                form.Show();
+
+                form.textBox_output_TextChanged();
+                form.Show();
+            
+
+
+        }
+
+        private void textBox_q_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_T_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+                
+    }
+}
---
+++ Processing.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw
+{
+    static class Processing
+    {
+        static public int q;
+        static public double a1;
+        static public double a2;
+        static public double b;
+        static public double T;
+        static public bool checkLimit;
+        static public Matrix x_0 = new Matrix(3, 1);
+
+        static public bool checkString( string s1, string s2, string s3, string s4, string s5,string s6,string s7,string s8)
+        {
+            return (s1.Length != 0 && s2.Length != 0 && s3.Length != 0 && s4.Length != 0 && s5.Length != 0 && s6.Length != 0 && s7.Length != 0 && s8.Length != 0);
+        }
+
+        static public void function()
+        {
+            Program.x.Clear();
+            Program.y.Clear();
+
+            int k_var = (Program.k > 4) ? Program.k / 4 : Program.k;
+            Matrix A = new Matrix(1, 1);
+            Matrix B = new Matrix(1, 1);
+            Matrix C = new Matrix(1, 1);
+            Matrix E = new Matrix(1, 1);
+            Matrix F = new Matrix(1, 1);
+            Matrix G = new Matrix(1, 1);
+            Matrix X = new Matrix(1, 1);
+
+            
+
+            A.inputByFile("inputA.txt");
+            B.inputByFile("inputB.txt");
+            C.inputByFile("inputC.txt");
+            E.inputByFile("inputE.txt");
+            A.M[2][1] = -a1;
+            A.M[2][2] = -a2;
+            B.M[2][0] = b;
+
+            F = F.make_F(A, T, q);
+          
+            G = G.make_G(A, B, T, q);
+          
+
+            int[] u;
+            init_u(Program.k, k_var, out u);
+
+            checkLimit = Matrix.checkLimit(A, Processing.T);
+            //Matrix.X_function_Y(F, G, u, T, C);
+            Matrix.X_function_Y_const(F, G, T, C);
+        }
+       
+        
+        public static void init_u(int k, int k_var, out int[] result)
+        {
+            result = new int[(int)k + 1];
+            for (int i = 0; i < k + 1; i++)
+            {
+                if (((i - 1) / k_var) % 2 == 0)
+                    result[i] = 1;
+                else result[i] = -1;
+
+            }
+        }
+        public static bool checkComponents(int q, double a1, double a2, double T)
+        {
+            return(((q<=10 && q>=1) &&( a1<=10 && a1>=1) &&(a2<=10 && a2>=1) && (T<=1 && T>=0.001)));
+        }
+    }
+}
